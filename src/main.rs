13948b637405c4128d8e04c820b2956b663caa71
GeoffNN <g.negiar@gmail.com>
2023-12-23 19:09:02 +0000

    Factored world creation in a separate function

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,7 @@ use eerdekens_bot::vec::Point3;
 use eerdekens_bot::camera::Camera;
 use eerdekens_bot::material;
 
-fn main() {
-    let image_path = "raytracing_level_12.ppm";
-
-    // Camera
-    let mut camera = Camera::default();
-    camera.aspect_ratio = 16.0 / 9.0;
-    camera.image_width = 400;
-    camera.num_samples_per_pixel = 100;
-    camera.max_depth = 50;
-    camera.vfov = 20.0;
-    camera.lookfrom = Point3::new(-2., 2., 1.);
-    camera.lookat = Point3::new(0., 0., -1.);
-    camera.v_up = Point3::new(0., 1., 0.);
-    camera.defocus_angle = 10.;
-    camera.focus_dist = 3.4;
-
-    // TODO(geoff): Take this from a config file
-
-    // Materials
-    // World
+fn three_balls_on_ground_scene() -> HittableList {
     let mut world = HittableList::new();
     world.add(Box::new(Sphere::new(
         Point3::new(0., 0., -1.),
@@ -46,6 +27,28 @@ fn main() {
         100.,
         &material::MATERIAL_GROUND,
     )));
+    return world;
+}
+
+fn main() {
+    let image_path = "raytracing_level_release_1.ppm";
+
+    // Camera
+    let mut camera = Camera::default();
+    camera.aspect_ratio = 16.0 / 9.0;
+    camera.image_width = 400;
+    camera.num_samples_per_pixel = 100;
+    camera.max_depth = 50;
+    camera.vfov = 20.0;
+    camera.lookfrom = Point3::new(-2., 2., 1.);
+    camera.lookat = Point3::new(0., 0., -1.);
+    camera.v_up = Point3::new(0., 1., 0.);
+    camera.defocus_angle = 10.;
+    camera.focus_dist = 3.4;
+
+    // World
+    // TODO(geoff): Take world from a config file
+    let world = three_balls_on_ground_scene();
 
     camera.render(&world, image_path);
 }
